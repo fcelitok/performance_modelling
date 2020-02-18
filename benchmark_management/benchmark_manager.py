a6ab050954519ae77711021a7b7c1051cec58aff
Enri2077 <enry2077@gmail.com>
2020-02-18 15:31:36 +0100

    got rid of Configuration class, improved launch args

--- benchmark_management/benchmark_manager.py
+++ benchmark_management/benchmark_manager.py
@@ -10,13 +10,33 @@ class BenchmarkManager(object):
     def __init__(self):
         pass
 
-    @staticmethod
-    def test():
-        c = Component('test_gmapping', 'test', 'gmapping.launch')
 
-        print(c.launchfile_path)
+def test():
+    roscore = Component('roscore', 'test', 'roscore.launch')
+    environment = Component('environment_stage', 'test', 'stage.launch')
+    slam = Component('slam_gmapping', 'test', 'gmapping.launch')
+    navigation = Component('navigation_move_base', 'test', 'move_base.launch')
 
-        c.launch(launch_args=['configuration:=/home/enrico/w/catkin_ws/src/test/config/gmapping_1.yaml', 'test_arg:=ASDASDASD'])
+    print("Components:")
+    print(' -', roscore.name, roscore.launchfile_path)
+    print(' -', environment.name, environment.launchfile_path)
+    print(' -', slam.name, slam.launchfile_path)
+    print(' -', navigation.name, navigation.launchfile_path)
+
+    roscore.launch()
+    environment.launch()
+    slam.launch(configuration='/home/enrico/w/catkin_ws/src/test/config/gmapping_1.yaml', test_arg='test_value')
+    navigation.launch()
+
+    print("Launch completed")
+
+    navigation.shutdown()
+    slam.shutdown()
+    environment.shutdown()
+    roscore.shutdown()
+
+    print("Shutdown completed")
 
 
-BenchmarkManager().test()
+if __name__ == '___main___':
+    test()
--- benchmark_management/component.py
+++ benchmark_management/component.py
@@ -8,21 +8,34 @@ class Component(object):
 
     def __init__(self, name, package_name, launchfile_name):
         self.name = name
-        self.package_name = package_name
-        self.launchfile_name = launchfile_name
-        self.launchfile_path = roslaunch.rlutil.resolve_launch_arguments([self.package_name, self.launchfile_name])[0]
+        self._package_name = package_name
+        self._launchfile_name = launchfile_name
+        self._launchfile_path = roslaunch.rlutil.resolve_launch_arguments([self._package_name, self._launchfile_name])[0]
 
-    def launch(self, configuration=None, launch_args=None):
-        assert(configuration is None or isinstance(configuration, Configuration))
-        assert(launch_args is None or isinstance(launch_args, list))
+        self.launch_parent = None
+
+    def launch(self, **args):
+        launch_args = list()
+
+        for arg_value in args.items():
+            launch_args.append(':='.join(arg_value))
 
         uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
         roslaunch.configure_logging(uuid)
-        launch = roslaunch.parent.ROSLaunchParent(uuid, [(self.launchfile_path, launch_args)])
-        launch.start()
-        rospy.loginfo("Component {} started")
+        self.launch_parent = roslaunch.parent.ROSLaunchParent(uuid, [(self._launchfile_path, launch_args)])
+        self.launch_parent.start()
 
+    def shutdown(self):
+        self.launch_parent.shutdown()
 
-class Configuration(object):
-    def __init__(self):
-        pass
+    @property
+    def package_name(self):
+        return self._package_name
+
+    @property
+    def launchfile_name(self):
+        return self._launchfile_name
+
+    @property
+    def launchfile_path(self):
+        return self._launchfile_path
