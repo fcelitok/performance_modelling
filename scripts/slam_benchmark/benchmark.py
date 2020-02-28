ded3815eb1650def5e2d3e09b4b38051c80cbcf6
Enri2077 <enry2077@gmail.com>
2020-02-28 17:14:14 +0100

    test all combinations of component configurations

--- scripts/slam_benchmark/benchmark.py
+++ scripts/slam_benchmark/benchmark.py
@@ -3,9 +3,12 @@
 
 from __future__ import print_function
 
+import rospkg
 import argparse
 import os
+import yaml
 from os import path
+import itertools
 
 from performance_modelling_ros import Component
 from performance_modelling_ros.utils import backup_file_if_exists, print_info
@@ -30,9 +33,6 @@ class BenchmarkRun(object):
         self.components_ros_output = 'screen' if show_ros_info else 'log'
         self.run_output_folder = run_output_folder
 
-        # run variables
-        self.supervisor = None
-
     def execute_run(self):
         # prepare folder structure
         backup_file_if_exists(self.run_output_folder)
@@ -56,7 +56,7 @@ class BenchmarkRun(object):
         slam = Component('gmapping', 'performance_modelling', 'gmapping.launch', slam_params)
         navigation = Component('move_base', 'performance_modelling', 'move_base.launch', navigation_params)
         explorer = Component('explore_lite', 'performance_modelling', 'explore_lite.launch', explorer_params)
-        self.supervisor = Component('supervisor', 'slam_benchmark_supervisor', 'supervisor.launch', supervisor_params)
+        supervisor = Component('supervisor', 'performance_modelling', 'slam_benchmark_supervisor.launch', supervisor_params)
 
         # launch components
         print_info("execute_run: launching components")
@@ -67,13 +67,13 @@ class BenchmarkRun(object):
         slam.launch()
         navigation.launch()
         explorer.launch()
-        self.supervisor.launch()
+        supervisor.launch()
 
         # TODO check if all components launched properly
 
         # wait for the supervisor component to finish
         print_info("execute_run: waiting for supervisor to finish")
-        self.supervisor.wait_to_finish()
+        supervisor.wait_to_finish()
         print_info("execute_run: supervisor has shutdown")
 
         # shutdown remaining components
@@ -94,7 +94,7 @@ class BenchmarkRun(object):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description='Runs slam benchmark')
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description='Execute the benchmark')
 
     parser.add_argument('-e', dest='environment_dataset_folder',
                         help='Dataset folder containg the stage environment.world file.',
@@ -102,10 +102,10 @@ if __name__ == '__main__':
                         default="~/ds/performance_modelling/airlab/",
                         required=False)
 
-    parser.add_argument('-c', dest='components_configuration_folder',
-                        help='Folder containing the configuration for each component.',
+    parser.add_argument('-c', dest='benchmark_configuration',
+                        help='Yaml file with the configuration of the benchmark.',
                         type=str,
-                        default="~/w/catkin_ws/src/performance_modelling/config/component_configurations/",
+                        default="~/w/catkin_ws/src/performance_modelling/config/benchmark_configurations/slam_benchmark_1.yaml",
                         required=False)
 
     parser.add_argument('-r', dest='base_run_folder',
@@ -115,7 +115,7 @@ if __name__ == '__main__':
                         required=False)
 
     parser.add_argument('-n', '--num-runs', dest='num_runs',
-                        help='Number of runs to be executed.',
+                        help='Number of runs to be executed for each combination of configurations.',
                         type=int,
                         default=1,
                         required=False)
@@ -133,35 +133,65 @@ if __name__ == '__main__':
     args = parser.parse_args()
     base_run_folder = path.expanduser(args.base_run_folder)
     environment_dataset_folder = path.expanduser(args.environment_dataset_folder)
-    components_configuration_folder = path.expanduser(args.components_configuration_folder)
+    benchmark_configuration = path.expanduser(args.benchmark_configuration)
+
+    rospack = rospkg.RosPack()
+    package_path = rospack.get_path('performance_modelling')
+    components_configurations_folder = path.join(package_path, "config/component_configurations/")
 
     if not path.exists(base_run_folder):
         os.makedirs(base_run_folder)
 
-    for _ in range(args.num_runs):
+    with open(benchmark_configuration, 'r') as f:
+        benchmark_configuration = yaml.load(f)
 
-        i = 0
-        run_folder = path.join(base_run_folder, "run_{run_number}".format(run_number=i))
-        while path.exists(run_folder):
-            i += 1
+    supervisor_configuration = path.join(components_configurations_folder, benchmark_configuration['supervisor_configuration'])
+
+    components_configurations_dict = benchmark_configuration['components_configurations']
+
+    # convert components configurations relative paths to absolute paths
+    for component, configurations_relative_path_list in components_configurations_dict.items():
+        components_configurations_dict[component] = map(lambda relative_path: path.join(components_configurations_folder, relative_path), configurations_relative_path_list)
+
+    # convert the dict with {component_name: [configuration_1, configuration_2]} to the list [(component_name, configuration_1), (component_name, configuration_2), ...]
+    configurations_alternatives = list()
+    for component, configurations_list in components_configurations_dict.items():
+        component_configuration_list_of_tuples = map(lambda configuration: (component, configuration), configurations_list)
+        configurations_alternatives.append(component_configuration_list_of_tuples)
+
+    # obtain the list of combinations from the list of alternatives
+    # example: component 1 with configurations [A, B, C, D], and component 2 with configurations [x, y]:
+    #   itertools.product([A, B, C, D], [x, y]) --> [[A, x], [A, y], [B, x], [B, y], [C, x], [C, y], [D, x], [D, y]]
+    configuration_combinations_lists = list(itertools.product(*configurations_alternatives))
+
+    # convert the list of lists to a list of dicts
+    configuration_combinations_dicts = map(dict, configuration_combinations_lists)
+
+    num_combinations = len(configuration_combinations_dicts)
+    print_info("number of configuration combinations: {}".format(num_combinations))
+    print_info("number of runs per combination: {}".format(args.num_runs))
+    print_info("total number of runs: {}".format(args.num_runs * num_combinations))
+
+    for components_configurations in configuration_combinations_dicts:
+        for _ in range(args.num_runs):
+
+            # find an available run folder path
+            i = 0
             run_folder = path.join(base_run_folder, "run_{run_number}".format(run_number=i))
+            while path.exists(run_folder):
+                i += 1
+                run_folder = path.join(base_run_folder, "run_{run_number}".format(run_number=i))
 
-        component_configurations = {
-            'gmapping': path.join(components_configuration_folder, "gmapping/gmapping_1.yaml"),
-            'move_base': path.join(components_configuration_folder, "move_base/move_base_1.yaml"),
-            'explore_lite': path.join(components_configuration_folder, "explore_lite/explore_lite_1.yaml"),
-        }
+            # instantiate and execute the run
+            print_info("benchmark: starting run {run_index}".format(run_index=i))
+            r = BenchmarkRun(run_output_folder=run_folder,
+                             show_ros_info=args.show_ros_info,
+                             headless=args.headless,
+                             stage_world_file=path.join(environment_dataset_folder, "environment.world"),
+                             component_configuration_files=components_configurations,
+                             supervisor_configuration_file=supervisor_configuration)
 
-        supervisor_configuration = path.join(components_configuration_folder, "slam_benchmark_supervisor/slam_benchmark_supervisor.yaml")
-
-        r = BenchmarkRun(run_output_folder=run_folder,
-                         show_ros_info=args.show_ros_info,
-                         headless=args.headless,
-                         stage_world_file=path.join(environment_dataset_folder, "environment.world"),
-                         component_configuration_files=component_configurations,
-                         supervisor_configuration_file=supervisor_configuration)
-
-        r.execute_run()
-        print_info("benchmark: run {run_index} completed".format(run_index=i))
+            r.execute_run()
+            print_info("benchmark: run {run_index} completed".format(run_index=i))
 
     print_info("benchmark: all runs completed")
