63a8dec1cb7097ec45b416ceedde568d2fa4b83d
Enri2077 <enry2077@gmail.com>
2020-03-18 16:33:57 +0100

    moved supervisor node in this repo and decoupled run class

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # for your packages to be recognized by python
 d = generate_distutils_setup(
- packages=['performance_modelling_ros'],
- package_dir={'performance_modelling_ros': 'src/performance_modelling_ros'}
+ packages=['performance_modelling_ros', 'slam_benchmark_supervisor_ros'],
+ package_dir={'performance_modelling_ros': 'src/performance_modelling_ros', 'slam_benchmark_supervisor_ros': 'src/slam_benchmark_supervisor_ros'}
 )
 
 setup(**d)
