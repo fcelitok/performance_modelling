f5037f724894de5a635696fadebc2a61d221e7d5
Enri2077 <enry2077@gmail.com>
2020-04-19 12:06:02 +0200

    added plot of metrics by metric, metric histograms, and improved terminal options

--- scripts/slam_benchmark/collect_results_slam_benchmark.py
+++ scripts/slam_benchmark/collect_results_slam_benchmark.py
@@ -42,10 +42,6 @@ def get_yaml(yaml_file_path):
         return yaml.load(yaml_file)
 
 
-def aggregation_function(metric_values_list):
-    return np.mean(metric_values_list)
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description='Execute the analysis of the benchmark results.')
 
@@ -62,14 +58,50 @@ if __name__ == '__main__':
                         required=False)
 
     parser.add_argument('-c', dest='cache_file',
-                        help='Cache file path. Defaults to ~/ds/performance_modelling_analysis_cache.pkl',
+                        help='If set the run data is cached and read from CACHE_FILE. CACHE_FILE defaults to ~/ds/performance_modelling_analysis_cache.pkl',
                         action='store_const',
                         const="~/ds/performance_modelling_analysis_cache.pkl",
                         default=None,
                         required=False)
 
     parser.add_argument('-i', dest='invalidate_cache',
-                        help='Invalidate the cache. Re-read the run data and update the cache file.',
+                        help='If set invalidate the cached run data. If set the run data is re-read and the cache file is updated.',
+                        action='store_true',
+                        default=False,
+                        required=False)
+
+    parser.add_argument('-a', dest='aggregation_function_name',
+                        help='Which function to use to aggregate performance values of multiple runs. Defaults to mean.',
+                        type=str,
+                        default='mean',
+                        required=False)
+
+    parser.add_argument('--plot-everything', dest='plot_everything',
+                        help='Plot everything.',
+                        action='store_true',
+                        default=False,
+                        required=False)
+
+    parser.add_argument('--plot-metrics-by-config', dest='plot_metrics_by_config',
+                        help='Plot metrics by config.',
+                        action='store_true',
+                        default=False,
+                        required=False)
+
+    parser.add_argument('--plot-metrics-by-parameter', dest='plot_metrics_by_parameter',
+                        help='Plot metrics by parameter using the specified aggregation function.',
+                        action='store_true',
+                        default=False,
+                        required=False)
+
+    parser.add_argument('--plot-metrics-by-metric', dest='plot_metrics_by_metric',
+                        help='Plot metrics by metric.',
+                        action='store_true',
+                        default=False,
+                        required=False)
+
+    parser.add_argument('--plot-metric-histograms', dest='plot_metric_histograms',
+                        help='Plot metric histograms.',
                         action='store_true',
                         default=False,
                         required=False)
@@ -92,6 +124,9 @@ if __name__ == '__main__':
     if not path.exists(output_folder):
         os.makedirs(output_folder)
 
+    aggregation_function_name = args.aggregation_function_name
+    aggregation_function = {'std': np.std, 'mean': np.mean, 'median': np.median, 'min': np.min, 'max': np.max}[aggregation_function_name]
+
     run_folders = filter(path.isdir, glob.glob(path.abspath(base_run_folder) + '/*'))
     print("base_run_folder:", base_run_folder)
 
@@ -110,20 +145,26 @@ if __name__ == '__main__':
         # collect results from each run
         print_info("reading run data")
         for i, run_folder in enumerate(run_folders):
+            run_record = dict()
             metric_results_folder = path.join(run_folder, "metric_results")
             run_info_file_path = path.join(run_folder, "run_info.yaml")
+
             if not path.exists(metric_results_folder):
                 print_error("metric_results_folder does not exists [{}]".format(metric_results_folder))
+
                 continue
             if not path.exists(run_info_file_path):
                 print_error("run_info file does not exists [{}]".format(run_info_file_path))
                 continue
 
-            run_record = dict()
-
             run_info = get_yaml(run_info_file_path)
 
-            gmapping_configuration_file_path = run_info['components_configuration']['gmapping']
+            if 'local_components_configuration' in run_info:
+                gmapping_configuration_file_path = path.join(run_folder, run_info['local_components_configuration']['gmapping'])
+            else:
+                # if the local path is not in the run info, use the (deprecated) absolute path of the original configuration file
+                gmapping_configuration_file_path = run_info['components_configuration']['gmapping']
+
             gmapping_configuration = get_yaml(gmapping_configuration_file_path)
 
             particles = gmapping_configuration['particles']
@@ -133,22 +174,44 @@ if __name__ == '__main__':
             config = (particles, delta, maxUrange, environment_name)
 
             run_record['config'] = config
-
-            explored_area_path = path.join(metric_results_folder, "normalised_explored_area.yaml")
-            if path.exists(explored_area_path):
-                metrics_by_config['normalised_explored_area'][config].append(get_yaml(explored_area_path)['normalised_explored_area'])
-                run_record['normalised_explored_area'] = get_yaml(explored_area_path)['normalised_explored_area']
-                metric_names.add('normalised_explored_area')
-
-                metrics_by_config['explored_area'][config].append(get_yaml(explored_area_path)['result_map']['area']['free'])
+            run_record['failure'] = 0
+            metric_names.add('failure')
+            # TODO log each failure mode
 
             trajectory_length_path = path.join(metric_results_folder, "trajectory_length")
             if path.exists(trajectory_length_path):
                 trajectory_length = float(get_simple_value(trajectory_length_path))
+                if trajectory_length < 3.0:
+                    metrics_by_config['failure'][config].append(1)
+                    run_record['failure'] = 1
+                    metrics_by_run.append(run_record)
+                    continue
                 metrics_by_config['trajectory_length'][config].append(trajectory_length)
                 run_record['trajectory_length'] = trajectory_length
                 metric_names.add('trajectory_length')
             else:
+                metrics_by_config['failure'][config].append(1)
+                run_record['failure'] = 1
+                metrics_by_run.append(run_record)
+                continue
+
+            explored_area_path = path.join(metric_results_folder, "normalised_explored_area.yaml")
+            if path.exists(explored_area_path):
+                normalised_explored_area = get_yaml(explored_area_path)['normalised_explored_area']
+                if normalised_explored_area < 0.1:
+                    metrics_by_config['failure'][config].append(1)
+                    run_record['failure'] = 1
+                    metrics_by_run.append(run_record)
+                    continue
+                metrics_by_config['normalised_explored_area'][config].append(normalised_explored_area)
+                run_record['normalised_explored_area'] = normalised_explored_area
+                metric_names.add('normalised_explored_area')
+
+                metrics_by_config['explored_area'][config].append(get_yaml(explored_area_path)['result_map']['area']['free'])
+            else:
+                metrics_by_config['failure'][config].append(1)
+                run_record['failure'] = 1
+                metrics_by_run.append(run_record)
                 continue
 
             absolute_error_path = path.join(metric_results_folder, "absolute_localisation_error", "absolute_localization_error")
@@ -192,6 +255,8 @@ if __name__ == '__main__':
             if path.exists(re_t_path):
                 metrics_by_config['normalised_re_t'][config].append(get_metric_evaluator_mean(re_t_path) / trajectory_length)
                 metrics_by_config['re_t'][config].append(get_metric_evaluator_mean(re_t_path))
+                run_record['re_t'] = get_metric_evaluator_mean(re_t_path)
+                metric_names.add('re_t')
 
             correction_ordered_r_path = path.join(metric_results_folder, "base_link_correction_poses", "ordered_r.csv")
             if path.exists(correction_ordered_r_path):
@@ -216,7 +281,10 @@ if __name__ == '__main__':
                 run_record['normalised_correction_re_t'] = get_metric_evaluator_mean(correction_re_t_path) / trajectory_length
                 metric_names.add('normalised_correction_re_t')
                 metrics_by_config['correction_re_t'][config].append(get_metric_evaluator_mean(correction_re_t_path))
+                run_record['correction_re_t'] = get_metric_evaluator_mean(correction_re_t_path)
+                metric_names.add('correction_re_t')
 
+            metrics_by_config['failure'][config].append(0)
             metrics_by_run.append(run_record)
             del run_record
 
@@ -243,8 +311,7 @@ if __name__ == '__main__':
             configs_sets['environment'].add(environment)
 
     # plot metrics grouped by configuration
-    plot_metrics_by_config = False
-    if plot_metrics_by_config:
+    if args.plot_everything or args.plot_metrics_by_config:
         print_info("plot metrics grouped by configuration")
         metrics_by_config_folder = path.join(output_folder, "metrics_by_config")
         if not path.exists(metrics_by_config_folder):
@@ -256,7 +323,7 @@ if __name__ == '__main__':
             ax.margins(0.15)
             x_ticks = list()
             for i, (config, metric_values) in enumerate(metrics_by_config[metric_name].items()):
-                ax.plot([i] * len(metric_values[metric_name]), metric_values[metric_name], marker='_', linestyle='', ms=20, color='black')
+                ax.plot([i] * len(metric_values), metric_values, marker='_', linestyle='', ms=20, color='black')
                 x_ticks.append(str(config))
 
             ax.set_title(metric_name)
@@ -267,11 +334,10 @@ if __name__ == '__main__':
             plt.close(fig)
 
     # plot metrics in function of single configuration parameters
-    plot_metrics_by_parameter = False
-    if plot_metrics_by_parameter:
+    if args.plot_everything or args.plot_metrics_by_parameter:
         print_info("plot metrics by parameter")
 
-        metrics_by_parameter_folder = path.join(output_folder, "metrics_by_parameter")
+        metrics_by_parameter_folder = path.join(output_folder, "metrics_by_parameter_using_{}".format(aggregation_function_name))
         if not path.exists(metrics_by_parameter_folder):
             os.makedirs(metrics_by_parameter_folder)
 
@@ -282,7 +348,7 @@ if __name__ == '__main__':
             for parameter_name in parameter_names:
                 # plot lines for same-parameter metric values
                 fig, ax = plt.subplots()
-                fig.set_size_inches(*cm_to_body_parts(20, 20))
+                fig.set_size_inches(*cm_to_body_parts(40, 40))
                 ax.margins(0.15)
                 ax.set_xlabel(parameter_name)
                 ax.set_ylabel(metric_name)
@@ -299,14 +365,13 @@ if __name__ == '__main__':
                     ax.plot(parameter_values, metric_values, marker='o', ms=5, label=str(other_parameter_values))
 
                 ax.legend()
-                fig.savefig(path.join(metrics_by_parameter_folder, "{}_by_{}.svg".format(metric_name, parameter_name)), bbox_inches='tight')
+                fig.savefig(path.join(metrics_by_parameter_folder, "{}_by_{}_using_{}.svg".format(metric_name, parameter_name, aggregation_function_name)), bbox_inches='tight')
                 plt.close(fig)
 
             print_info("plot metrics by parameter: {}%".format((i + 1)*100/len(metrics_by_config.keys())), replace_previous_line=True)
 
     # plot metrics in function of other metrics
-    plot_metrics_by_metric = True
-    if plot_metrics_by_metric:
+    if args.plot_everything or args.plot_metrics_by_metric:
         print_info("plot metrics by metric")
 
         metrics_by_metric_folder = path.join(output_folder, "metrics_by_metric")
@@ -341,6 +406,38 @@ if __name__ == '__main__':
                 plt.close(fig)
 
                 progress_counter += 1
-                print_info("plot metrics by parameter: {}%".format(progress_counter * 100 / progress_total), replace_previous_line=True)
+                print_info("plot metrics by metric: {}%".format(progress_counter * 100 / progress_total), replace_previous_line=True)
+
+    # plot metrics in function of other metrics
+    if args.plot_everything or args.plot_metric_histograms:
+        print_info("plot metric histograms")
+
+        metric_histograms_folder = path.join(output_folder, "metric_histograms")
+        if not path.exists(metric_histograms_folder):
+            os.makedirs(metric_histograms_folder)
+
+        progress_counter = 0
+        progress_total = len(metric_names)
+
+        for metric_x_name in metric_names:
+            fig, ax = plt.subplots()
+            fig.set_size_inches(*cm_to_body_parts(40, 40))
+            ax.margins(0.15)
+            ax.set_xlabel(metric_x_name)
+
+            metric_values = list()
+            for run_record in metrics_by_run:
+                if metric_x_name in run_record:
+                    metric_values.append(run_record[metric_x_name])
+
+            # plot histogram graph for metric x values
+            ax.hist(metric_values, bins=100)
+
+            fig_name = "{}.svg".format(metric_x_name)
+            fig.savefig(path.join(metric_histograms_folder, fig_name), bbox_inches='tight')
+            plt.close(fig)
+
+            progress_counter += 1
+            print_info("plot metric histograms: {}%".format(progress_counter * 100 / progress_total), replace_previous_line=True)
 
     # TODO plot metric sensitivity by param
