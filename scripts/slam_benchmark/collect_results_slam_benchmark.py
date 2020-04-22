7c958a0a744d38fc49c55850913a09e0a94ef7a3
Enri2077 <enry2077@gmail.com>
2020-04-22 02:43:49 +0200

    finished improving plots and data collection

--- scripts/slam_benchmark/collect_results_slam_benchmark.py
+++ scripts/slam_benchmark/collect_results_slam_benchmark.py
@@ -10,8 +10,8 @@ import pickle
 import sys
 import yaml
 import pandas as pd
-import numpy as np
-from collections import defaultdict
+import pandas.core.groupby
+from matplotlib.cm import get_cmap
 from os import path
 
 import matplotlib.pyplot as plt
@@ -22,9 +22,9 @@ from performance_modelling_ros.utils import print_info, print_error
 def cm_to_body_parts(*argv):
     inch = 2.54
     if isinstance(argv[0], tuple):
-        return tuple(x / inch for x in argv[0])
+        return tuple(x_cm / inch for x_cm in argv[0])
     else:
-        return tuple(x / inch for x in argv)
+        return tuple(x_cm / inch for x_cm in argv)
 
 
 def get_simple_value(result_path):
@@ -59,9 +59,7 @@ if __name__ == '__main__':
 
     parser.add_argument('-c', dest='cache_file',
                         help='If set the run data is cached and read from CACHE_FILE. CACHE_FILE defaults to ~/ds/performance_modelling_analysis_cache.pkl',
-                        action='store_const',
-                        const="~/ds/performance_modelling_analysis_cache.pkl",
-                        default=None,
+                        default="~/ds/performance_modelling_analysis_cache.pkl",
                         required=False)
 
     parser.add_argument('-i', dest='invalidate_cache',
@@ -76,20 +74,14 @@ if __name__ == '__main__':
                         default=False,
                         required=False)
 
-    parser.add_argument('--plot-metrics-by-config', dest='plot_metrics_by_config',
-                        help='Plot metrics by config.',
-                        action='store_true',
-                        default=False,
-                        required=False)
-
     parser.add_argument('--plot-metrics-by-parameter', dest='plot_metrics_by_parameter',
                         help='Plot metrics by parameter using the specified aggregation function.',
                         action='store_true',
                         default=False,
                         required=False)
 
-    parser.add_argument('--plot-metrics-by-metric', dest='plot_metrics_by_metric',
-                        help='Plot metrics by metric.',
+    parser.add_argument('--plot-trade-off', dest='plot_trade_off',
+                        help='Plot trade-off between metrics by environment.',
                         action='store_true',
                         default=False,
                         required=False)
@@ -125,12 +117,8 @@ if __name__ == '__main__':
         print_info("reading run data from cache")
         with open(cache_file_path) as f:
             cache = pickle.load(f)
-        # metrics_by_config = cache['metrics_by_config']
-        metrics_by_run = cache['metrics_by_run']
         df = cache['df']
     else:
-        # metrics_by_config = defaultdict(lambda: defaultdict(list))
-        metrics_by_run = list()
         df = pd.DataFrame()
 
         # collect results from each run
@@ -155,20 +143,15 @@ if __name__ == '__main__':
                 # if the local path is not in the run info, use the (deprecated) absolute path of the original configuration file
                 gmapping_configuration_file_path = run_info['components_configuration']['gmapping']
 
-            gmapping_configuration = get_yaml(gmapping_configuration_file_path)
-
-            particles = gmapping_configuration['particles']
-            delta = gmapping_configuration['delta']
-            maxUrange = gmapping_configuration['maxUrange']
-            environment_name = path.basename(run_info['environment_folder'])
-            config_tuple = (particles, delta, maxUrange, environment_name)
-
             run_record = dict()
-            metrics_by_run.append(run_record)
 
-            run_record['config'] = config_tuple
-            run_record['failure'] = 0
-            # TODO log each failure mode
+            gmapping_configuration = get_yaml(gmapping_configuration_file_path)
+            run_record['particles'] = gmapping_configuration['particles']
+            run_record['delta'] = gmapping_configuration['delta']
+            run_record['maxUrange'] = gmapping_configuration['maxUrange']
+            run_record['environment'] = path.basename(run_info['environment_folder'])
+
+            run_record['failure_rate'] = 0
 
             try:
                 run_events = get_csv(path.join(benchmark_data_folder, "run_events.csv"))
@@ -177,85 +160,67 @@ if __name__ == '__main__':
                 navigation_metrics = get_yaml(path.join(metric_results_folder, "navigation_metrics.yaml"))
                 computation_metrics = get_yaml(path.join(metric_results_folder, "computation_metrics.yaml"))
             except IOError as e:
-                # metrics_by_config['failure'][config_tuple].append(1)
-                run_record['failure'] = 1
+                run_record['failure_rate'] = 1
                 df = df.append(run_record, ignore_index=True)
                 continue
 
             trajectory_length = localisation_metrics['trajectory_length']
             if trajectory_length < 3.0 or trajectory_length is None:
-                # metrics_by_config['failure'][config_tuple].append(1)
-                run_record['failure'] = 1
+                run_record['failure_rate'] = 1
                 df = df.append(run_record, ignore_index=True)
                 continue
 
             normalised_explored_area = map_metrics['explored_area']['normalised_explored_area']
             if normalised_explored_area < 0.1 or normalised_explored_area is None:
-                # metrics_by_config['failure'][config_tuple].append(1)
-                run_record['failure'] = 1
+                run_record['failure_rate'] = 1
                 df = df.append(run_record, ignore_index=True)
                 continue
 
-            # metrics_by_config['normalised_explored_area'][config_tuple].append(normalised_explored_area)
             run_record['normalised_explored_area'] = normalised_explored_area
 
-            # metrics_by_config['mean_absolute_correction_error'][config_tuple].append(localisation_metrics['absolute_localization_error']['mean'])
             run_record['mean_absolute_correction_error'] = localisation_metrics['absolute_localization_error']['mean']
 
             run_start_time = float(run_events[run_events["event"] == "run_start"]["timestamp"])
             supervisor_finish_time = float(run_events[run_events["event"] == "supervisor_finished"]["timestamp"])
             run_execution_time = supervisor_finish_time - run_start_time
-            # metrics_by_config['run_execution_time'][config_tuple].append(run_execution_time)
             run_record['run_execution_time'] = run_execution_time
 
             normalised_gmapping_cpu_time = computation_metrics['cpu_and_memory_usage']['slam_gmapping_accumulated_cpu_time'] / run_execution_time
-            # metrics_by_config['normalised_slam_cpu_time'][config_tuple].append(normalised_gmapping_cpu_time)
             run_record['normalised_slam_cpu_time'] = normalised_gmapping_cpu_time
 
             explored_area = map_metrics['explored_area']['result_map']['area']['free']
             normalised_gmapping_uss = computation_metrics['cpu_and_memory_usage']['slam_gmapping_uss'] / explored_area
-            # metrics_by_config['normalised_slam_memory'][config_tuple].append(normalised_gmapping_uss)
             run_record['normalised_slam_memory'] = normalised_gmapping_uss
 
-            # metrics_by_config['failure'][config_tuple].append(0)
             df = df.append(run_record, ignore_index=True)
-            del run_record
 
             print_info("reading run data: {}%".format((i + 1)*100/len(run_folders)), replace_previous_line=True)
 
         # save cache
         if cache_file_path is not None:
             # metrics_by_config = dict(metrics_by_config)
-            cache = {'df': df, 'metrics_by_run': metrics_by_run}
+            cache = {'df': df}
             with open(cache_file_path, 'w') as f:
                 pickle.dump(cache, f)
 
-    metric_names = set(df.drop('config', axis=1).columns)
-
-    metrics_by_config = dict()
-    for metric_name in metric_names:
-        metrics_by_config[metric_name] = dict()
-        for config, df_grouped_by_config in df.groupby(by='config'):
-            metrics_by_config[metric_name][tuple(list(config))] = df_grouped_by_config[metric_name].tolist()
-
     parameter_names = ('particles', 'delta', 'maxUrange', 'environment')
-    configs_sets = defaultdict(set)
-    metrics_x_y_by_config = set()
-    for metric_name in metrics_by_config.keys():
-        for config_tuple in metrics_by_config[metric_name].keys():
-            metrics_x_y_by_config.add(config_tuple)
-
-            particles, delta, maxUrange, environment = config_tuple
-            configs_sets['particles'].add(particles)
-            configs_sets['delta'].add(delta)
-            configs_sets['maxUrange'].add(maxUrange)
-            configs_sets['environment'].add(environment)
+    metric_optimisation_factor = {'normalised_explored_area': 1,
+                                  'failure_rate': -1,
+                                  'mean_absolute_correction_error': -1,
+                                  'run_execution_time': -1,
+                                  'normalised_slam_cpu_time': -1,
+                                  'normalised_slam_memory': -1}
+    metric_names = set(df.columns) - set(parameter_names)
 
     # plot metrics in function of single configuration parameters
     if args.plot_everything or args.plot_metrics_by_parameter:
-        aggregation_function = {'std': np.std, 'mean': np.mean, 'median': np.median, 'min': np.min, 'max': np.max}
-        for aggregation_function_name in aggregation_function.keys():
+        aggregation_functions = {'var': pd.core.groupby.DataFrameGroupBy.var,
+                                 'mean': pd.core.groupby.DataFrameGroupBy.mean,
+                                 'median': pd.core.groupby.DataFrameGroupBy.median,
+                                 'min': pd.core.groupby.DataFrameGroupBy.min,
+                                 'max': pd.core.groupby.DataFrameGroupBy.max}
 
+        for aggregation_function_name, aggregation_function in aggregation_functions.items():
             print_info("plot metrics by parameter using {a}".format(a=aggregation_function_name))
             metrics_by_parameter_folder = path.join(output_folder, "metrics_by_parameter_using_{a}".format(a=aggregation_function_name))
             if not path.exists(metrics_by_parameter_folder):
@@ -263,46 +228,35 @@ if __name__ == '__main__':
 
             for i, metric_name in enumerate(metric_names):
 
-                configs_df = pd.DataFrame.from_records(columns=parameter_names, data=df[pd.notnull(df[metric_name])]['config'].unique().tolist())
-
-                for parameter_name in parameter_names:
+                for primary_parameter in parameter_names:
                     # plot lines for same-parameter metric values
                     fig, ax = plt.subplots()
                     fig.set_size_inches(*cm_to_body_parts(40, 40))
                     ax.margins(0.15)
-                    ax.set_xlabel(parameter_name)
+                    ax.set_xlabel(primary_parameter)
                     ax.set_ylabel(metric_name)
 
-                    other_parameters = list(set(parameter_names) - {parameter_name})
-                    grouped_parameter_values = configs_df.sort_values(by=other_parameters).groupby(other_parameters)
-                    for p, other_parameters_fixed in list(grouped_parameter_values):
+                    secondary_parameters = list(set(parameter_names) - {primary_parameter})
+                    for _, secondary_df in list(df.groupby(secondary_parameters, as_index=False)):
+                        sorted_secondary_df = secondary_df.sort_values(by=primary_parameter)
 
-                        other_parameters_fixed_sorted = other_parameters_fixed.sort_values(by=parameter_name)
-                        other_parameter_values = next(other_parameters_fixed_sorted[other_parameters].itertuples(index=False, name='config'))
-                        parameter_values = other_parameters_fixed_sorted[parameter_name]
-
-                        metric_values = list()
-                        for config_row in other_parameters_fixed_sorted.itertuples(index=False):
-                            config_tuple = tuple(list(config_row))
-                            metric_value = aggregation_function[aggregation_function_name](df[df['config'] == config_tuple][metric_name])
-                            metric_values.append(metric_value)
-
-                        ax.plot(parameter_values, metric_values, marker='o', ms=5, label=str(other_parameter_values))
+                        environment_df = sorted_secondary_df.groupby(parameter_names, as_index=False)
+                        aggregated_df = aggregation_function(environment_df)
+                        ax.plot(aggregated_df[primary_parameter], aggregated_df[metric_name], marker='o', ms=3)
 
                     ax.grid(color='black', alpha=0.5, linestyle='solid')
-                    ax.legend()
-                    fig.savefig(path.join(metrics_by_parameter_folder, "{}_by_{}_using_{}.svg".format(metric_name, parameter_name, aggregation_function_name)), bbox_inches='tight')
+                    fig.savefig(path.join(metrics_by_parameter_folder, "{}_by_{}_using_{}.svg".format(metric_name, primary_parameter, aggregation_function_name)), bbox_inches='tight')
                     plt.close(fig)
 
                 print_info("plot metrics by parameter using {a}: {p}%".format(a=aggregation_function_name, p=(i + 1)*100/len(metric_names)), replace_previous_line=True)
 
-    # plot metrics in function of other metrics
-    if args.plot_everything or args.plot_metrics_by_metric:
-        print_info("plot metrics by metric")
+    # plot trade-off between metrics
+    if args.plot_everything or args.plot_trade_off:
+        print_info("plot trade-off")
 
-        metrics_by_metric_folder = path.join(output_folder, "metrics_by_metric")
-        if not path.exists(metrics_by_metric_folder):
-            os.makedirs(metrics_by_metric_folder)
+        trade_off_folder = path.join(output_folder, "trade_off")
+        if not path.exists(trade_off_folder):
+            os.makedirs(trade_off_folder)
 
         progress_counter = 0
         progress_total = len(metric_names)**2 - len(metric_names)
@@ -316,27 +270,53 @@ if __name__ == '__main__':
                 ax.set_xlabel(metric_x_name)
                 ax.set_ylabel(metric_y_name)
 
-                # get the configurations associated with metric x and y, grouped by config
-                metrics_x_y_by_config = defaultdict(lambda: {'x': list(), 'y': list()})
-                for run_record in metrics_by_run:
-                    if metric_x_name in run_record and metric_y_name in run_record:
-                        metrics_x_y_by_config[run_record['config']]['x'].append(run_record[metric_x_name])
-                        metrics_x_y_by_config[run_record['config']]['y'].append(run_record[metric_y_name])
+                x_s = metric_optimisation_factor[metric_x_name]
+                y_s = metric_optimisation_factor[metric_y_name]
 
-                # plot scatter graph for same-config metric x, y values (each config has a different color)
-                for config_tuple, metric_values in metrics_x_y_by_config.items():
-                    ax.scatter(metric_values['x'], metric_values['y'])
+                df_grouped_by_environment = df.groupby('environment', as_index=False)
+                color_map = get_cmap(lut=len(df_grouped_by_environment), name=None)
+                for environment_index, (environment, environment_df) in enumerate(df_grouped_by_environment):
 
+                    metric_x_values = list()
+                    metric_y_values = list()
+                    for metric_x_value, metric_y_value in environment_df.groupby(parameter_names, as_index=False).mean()[[metric_x_name, metric_y_name]].itertuples(index=False):
+                        if pd.notnull(metric_x_value) and pd.notnull(metric_y_value):
+                            metric_x_values.append(metric_x_value)
+                            metric_y_values.append(metric_y_value)
+
+                    ax.scatter(metric_x_values, metric_y_values, color=color_map(environment_index), alpha=0.5)
+
+                    xy = zip(metric_x_values, metric_y_values)
+                    dominant_points = list()
+                    for x, y in xy:
+                        dominated = False
+                        for x_prime, y_prime in xy:
+                            if x_s*x_prime >= x_s*x and y_s*y_prime >= y_s*y and (x_prime, y_prime) != (x, y):
+                                dominated = True
+                                break
+                        if dominated:
+                            continue
+                        else:
+                            dominant_points.append((x, y))
+
+                    if len(dominant_points) > 0:
+                        dominant_points = sorted(dominant_points, key=lambda p: p[0])
+                        dominant_points_x, dominant_points_y = zip(*dominant_points)  # unzip
+                    else:
+                        dominant_points_x, dominant_points_y = max(xy, key=lambda x, y: (x_s*x, y_s*y))  # if no point is dominant, it means all points are aligned, so we can just take the maximum point (maximising both x and y)
+
+                    ax.plot(dominant_points_x, dominant_points_y, marker='x', mew=3, ms=10, color=color_map(environment_index), label=environment)
+
+                ax.legend()
                 ax.grid(color='black', alpha=0.5, linestyle='solid')
-
                 fig_name = "{x}_to_{y}.svg".format(y=metric_y_name, x=metric_x_name)
-                fig.savefig(path.join(metrics_by_metric_folder, fig_name), bbox_inches='tight')
+                fig.savefig(path.join(trade_off_folder, fig_name), bbox_inches='tight')
                 plt.close(fig)
 
                 progress_counter += 1
-                print_info("plot metrics by metric: {}%".format(progress_counter * 100 / progress_total), replace_previous_line=True)
+                print_info("plot trade-off: {}%".format(progress_counter * 100 / progress_total), replace_previous_line=True)
 
-    # plot metrics in function of other metrics
+    # plot metrics histograms
     if args.plot_everything or args.plot_metric_histograms:
         print_info("plot metric histograms")
 
@@ -353,10 +333,7 @@ if __name__ == '__main__':
             ax.margins(0.15)
             ax.set_xlabel(metric_x_name)
 
-            metric_values = list()
-            for run_record in metrics_by_run:
-                if metric_x_name in run_record:
-                    metric_values.append(run_record[metric_x_name])
+            metric_values = df[metric_x_name][pd.notnull(df[metric_x_name])]
 
             # plot histogram graph for metric x values
             ax.hist(metric_values, bins=100)
