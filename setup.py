aa5663d9dffddf5857242c947ca073b073956905
Enri2077 <enry2077@gmail.com>
2020-09-18 21:29:23 +0200

    advancing version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="performance-modelling-py",
-    version="0.0.2",
+    version="0.0.3",
     author="Enrico Piazza",
     author_email="erico.piazza@polimi.it",
     description="TODO",
